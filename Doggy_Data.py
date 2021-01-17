0c0d057a68f0f5dadfded508ce8cb24d006694ea
Lingyuan Yan <lingyuanyan@hotmail.com>
2021-01-16 22:46:04 -0800

    ya

---
+++ Doggy_Data.py
@@ -0,0 +1,35 @@
+import sqlite3
+import psycopg2
+from django.conf import settings
+import os
+from datetime import date
+import time
+import sys
+import csv
+import re
+import traceback
+def DoggyData(drop_table):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DoggyApp.settings')
+    # conn = sqlite3.connect('WC.db.sqlite')]
+    dbsettings = settings.DATABASES['default']
+
+    conn = psycopg2.connect(host=dbsettings["HOST"], database=dbsettings["NAME"],
+                            user=dbsettings["USER"], password=dbsettings["PASSWORD"])
+    cur = conn.cursor()
+    if(drop_table == 'yes') :
+        cur.execute('''
+        DROP TABLE IF EXISTS Doggy_Info
+        ''')
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Doggy_Info (
+        id SERIAL PRIMARY KEY,
+        pic TEXT,
+        caption  TEXT,
+        info TEXT
+        )
+    ''')
+    conn.commit()
+    print("...")
+    cur.close()
+    print("Your Data has been successfully uploaded")
+DoggyData('N/A')
