d1231eae4f51f9346a29a2dbc8eefbcf2ae06d30
AlexCao-html <caobubo@gmail.com>
2021-01-19 11:20:44 -0800

    fixed some stuff

--- frontend/src/components/Content.vue
+++ frontend/src/components/Content.vue
@@ -1,55 +1,12 @@
 <template>
   <div id="content">
     <h2>Content</h2>
-    <div class="container">
-      <div class="row">
-        <DogImage
-          class="col-sm"
-          name="me"
-          path="http://lingyuanyan.com/static/lingyuanyanapp/Images/Portrait.jpg"
-          caption="random dog"
-          des="random dog pic I found online"
-        >
-        </DogImage>
-        <DogImage
-          class="col-sm"
-          name="me"
-          path="http://lingyuanyan.com/static/lingyuanyanapp/Images/Portrait.jpg"
-          caption="random dog"
-          des="random dog pic I found online"
-        >
-        </DogImage>
-      </div>
-      <div class="row">
-        <DogImage
-          class="col-sm"
-          name="me"
-          path="http://lingyuanyan.com/static/lingyuanyanapp/Images/Portrait.jpg"
-          caption="random dog"
-          des="random dog pic I found onlinerandom dog pic I found onlinerandom dog pic I found online"
-        >
-        </DogImage>
-        <DogImage
-          name="me"
-          class="col-sm"
-          path="http://lingyuanyan.com/static/lingyuanyanapp/Images/Portrait.jpg"
-          caption="random dog"
-          des="random dog pic I found online"
-        >
-        </DogImage>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import DogImage from "./DogImage.vue";
-
 export default {
   name: "Content",
-  components: {
-    DogImage,
-  },
 };
 </script>
 
