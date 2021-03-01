8b32d340dfc43b86b218026731db868967dedd04
AlexCao-html <caobubo@gmail.com>
2021-02-28 18:59:23 -0800

    _

--- frontend/src/components/Dogs/Footer.vue
+++ frontend/src/components/Dogs/Footer.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="footer">
-    <hr>
     <h2>Footer</h2>
-    <p>made by the group turtlemaster</p>
+    <!-- <hr /> -->
+    <div class="text">
+      <p>Made by the turtlemaster group</p>
+    </div>
     <div class="others">
-    <!--  <p>Other sites: </p>
+      <p>Other sites: </p>
     <ul>
       <li><a href="http://covidhub.io" target="_blank">covidhub.io</a></li>
-    </ul>-->
+    </ul>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -18,7 +21,33 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->	<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  li {
+    text-align: justify;
+  }
+  #footer {
+    margin: 0 1vw 1vh;
+    border: 1px solid #aaa;
+    padding: 0;
+  }
+  h2 {
+    background: hsl(200, 65%, 75%);
+  }
+  a {
+    text-decoration: none;
+    color: #55f;
+  }
+  a:hover {
+    text-decoration: underline;
+    color: #00b;
+  }
+  .others {
+    background: hsl(200, 60%, 80%);
+  }
+  .text {
+    margin: 0 15px 10px;
+    padding-top: 20px;
+  }
 </style>
